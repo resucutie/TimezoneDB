589b7faaf43f92bbb690811087d69da1fa73297d
abUwUser <pedro.santos.cartaxo@gmail.com>
2021-12-26 23:23:13 -0300

    fix bug where the login button didn't work and an error page exists now

--- ui/main/index.js
+++ ui/main/index.js
@@ -26,50 +26,61 @@ const editCurrentUser = async (settings) => await fetch(url + "/api/user", {
 }).then(res => res.json())
 
 window.onload = async () => {
-    //other
-    const options = document.querySelector(".options")
-    //buttons
-    const loginButton = document.querySelector(".login")
-    const logoutButton = document.querySelector(".logout")
-    const getUserButton = document.querySelector(".get-user")
-    const changeButton = document.querySelector(".change")
-    //inputs
-    const editTimezoneInput = document.querySelector(".edit-timezone")
+    try {
+        //other
+        const options = document.querySelector(".options")
+        //buttons
+        const loginButton = document.querySelector(".login")
+        console.log(loginButton)
+        const logoutButton = document.querySelector(".logout")
+        const getUserButton = document.querySelector(".get-user")
+        const changeButton = document.querySelector(".change")
+        //inputs
+        const editTimezoneInput = document.querySelector(".edit-timezone")
 
 
-    // logic
-    const logged = await isLogged()
+        // logic
+        const logged = await isLogged()
 
-    loginButton.hidden = logged
-    logoutButton.hidden = !logged
-    options.hidden = !logged
+        loginButton.hidden = logged
+        logoutButton.hidden = !logged
+        options.hidden = !logged
 
-    const user = logged ? await getCurrentUser() : null
-    editTimezoneInput.value = user.timezone
+        const user = logged ? await getCurrentUser() : null
+        if (logged) editTimezoneInput.value = user.timezone
 
-    loginButton.addEventListener('click', async () => {
-        window.location.href = '/api/auth'
-    })
+        loginButton.addEventListener('click', async () => {
+            window.location.href = '/api/auth'
+        })
 
-    logoutButton.addEventListener('click', async () => {
-        window.location.href = `/api/auth/logout?redirecturl=${encodeURIComponent(url)}`
-    })
+        logoutButton.addEventListener('click', async () => {
+            window.location.href = `/api/auth/logout?redirecturl=${encodeURIComponent(url)}`
+        })
 
-    getUserButton.addEventListener('click', async () => {
-        console.log(user)
-    })
+        getUserButton.addEventListener('click', async () => {
+            console.log(user)
+        })
 
-    changeButton.addEventListener('click', async () => {
-        let settingsToPush = {}
-        if (editTimezoneInput.value !== "") settingsToPush.timezone = editTimezoneInput.value
+        changeButton.addEventListener('click', async () => {
+            let settingsToPush = {}
+            if (editTimezoneInput.value !== "") settingsToPush.timezone = editTimezoneInput.value
 
-        if (Object.keys(settingsToPush).length !== 0) {
-            console.log(await editCurrentUser(settingsToPush))
-            window.location.reload()
-        }
-    })
+            if (Object.keys(settingsToPush).length !== 0) {
+                console.log(await editCurrentUser(settingsToPush))
+                window.location.reload()
+            }
+        })
 
-    editTimezoneInput.addEventListener('input', () => {
-        editTimezoneInput.value = editTimezoneInput.value.replace(/[^\d.+-:]/g, '')
-    })
+        editTimezoneInput.addEventListener('input', () => {
+            editTimezoneInput.value = editTimezoneInput.value.replace(/[^\d.+-:]/g, '')
+        })
+    } catch (err) {
+        console.error(err)
+        document.body.innerHTML = `
+        <h1>an horrible error happened</h1>
+        <div>pls contact A user#8169</div><br><br>
+        <div style="color: #f00">
+            <b>error stack:</b> ${err.stack}
+        </div>`
+    }
 }
