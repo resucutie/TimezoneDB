2c880eced22c9e506b5713edcb8993972b3c8f30
abUwUser <pedro.santos.cartaxo@gmail.com>
2021-12-28 14:28:00 -0300

    revamped gui

--- ui/main/index.js
+++ ui/main/index.js
@@ -25,19 +25,49 @@ const editCurrentUser = async (settings) => await fetch(url + "/api/user", {
     body: JSON.stringify(settings)
 }).then(res => res.json())
 
+function setCookie(name, value, days = 120) {
+    var expires = ""
+    if (days) {
+        var date = new Date()
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000))
+        expires = "; expires=" + date.toUTCString()
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/"
+}
+function getCookie(name) {
+    var nameEQ = name + "="
+    var ca = document.cookie.split(';')
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i]
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length)
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length)
+    }
+    return null
+}
+
 window.onload = async () => {
     try {
         //other
-        const options = document.querySelector(".options")
+        const options = document.querySelector(".options-card")
+        const customTz = document.querySelector(".custom-timezone")
         //buttons
         const loginButton = document.querySelector(".login")
         const logoutButton = document.querySelector(".logout")
-        const getUserButton = document.querySelector(".get-user")
-        const tzIndicator = document.querySelector(".select-timezone-indicator")
         const changeButton = document.querySelector(".change")
+        const darkMode = document.querySelector(".dark-mode")
         //inputs
         const editTimezoneInput = document.querySelector(".edit-timezone")
+        //selectors
+        const tzSelector = document.querySelector(".select-timezone")
+        const tzIndicator = document.querySelector(".select-timezone-indicator")
 
+        function updateTheme() {
+            let cookie = getCookie("dark_mode")
+            if (!cookie) cookie = window.matchMedia?.('(prefers-color-scheme: dark)')?.matches ? "yes" : "no"
+            document.documentElement.className = cookie === "yes" ? "theme-dark" : "theme-light"
+        }
+
+        updateTheme()
 
         // logic
         const logged = await isLogged()
@@ -45,12 +75,38 @@ window.onload = async () => {
         loginButton.hidden = logged
         logoutButton.hidden = !logged
         options.hidden = !logged
+        customTz.hidden = true
+
+        let tz
 
         const user = logged ? await getCurrentUser() : null
         if (logged) {
-            editTimezoneInput.value = user.timezone.substring(1)
-            tzIndicator.value = user.timezone.charAt(0)
+            tz = user.timezone
+            editTimezoneInput.value = tz.substring(1)
+            tzIndicator.value = tz.charAt(0)
+            let a = false
+            for (const opt of tzSelector.options) {
+                if (opt.innerText.replace(/ /g, "").replace("\n", "") == "") tzSelector.removeChild(opt)
+                if (opt.value === tz && !a) {
+                    tzSelector.value = tz
+                    a = true
+                }
+            }
+            if (!a) {
+                tzSelector.value = "custom"
+                customTz.hidden = false
+            }
         }
+
+        tzSelector.addEventListener('change', () => {
+            if (tzSelector.value === "custom") {
+                customTz.hidden = false
+                return
+            }
+            customTz.hidden = true
+            tz = tzSelector.value
+            console.log(tz)
+        })
 
         loginButton.addEventListener('click', async () => {
             window.location.href = '/api/auth'
@@ -60,23 +116,28 @@ window.onload = async () => {
             window.location.href = `/api/auth/logout?redirecturl=${encodeURIComponent(url)}`
         })
 
-        getUserButton.addEventListener('click', async () => {
-            console.log(user)
+        darkMode.addEventListener('click', () => {
+            let cookie = getCookie("dark_mode")
+            if (!cookie) cookie = window.matchMedia?.('(prefers-color-scheme: dark)')?.matches ? "yes" : "no"
+            setCookie("dark_mode", cookie === "yes" ? "no" : "yes")
+            updateTheme()
+        })
+
+        editTimezoneInput.addEventListener('input', () => {
+            editTimezoneInput.value = editTimezoneInput.value.replace(/[^\d.:]/g, '')
+            if (!(editTimezoneInput.value === "" || Number.isNaN(Number(editTimezoneInput.value)))) tz = String(tzIndicator.value) + String(editTimezoneInput.value)
+            console.log(tz)
         })
 
         changeButton.addEventListener('click', async () => {
             let settingsToPush = {}
-            if (editTimezoneInput.value === "") { alert("Please put a timezone"); editTimezoneInput.select(); return}
-            settingsToPush.timezone = String(tzIndicator.value) + String(editTimezoneInput.value)
+            if (editTimezoneInput.value === "" || Number.isNaN(Number(tz))) { alert("Please put a timezone"); editTimezoneInput.select(); return}
+            settingsToPush.timezone = tz
 
             if (Object.keys(settingsToPush).length !== 0) {
                 console.log(await editCurrentUser(settingsToPush))
                 window.location.reload()
             }
-        })
-
-        editTimezoneInput.addEventListener('input', () => {
-            editTimezoneInput.value = editTimezoneInput.value.replace(/[^\d.+-:]/g, '')
         })
     } catch (err) {
         console.error(err)
